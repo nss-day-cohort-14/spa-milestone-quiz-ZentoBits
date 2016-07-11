7d0ca7ad0f8d18495059ff9cb554a59e7d63497b
Zachary George <zacharygeorge@Zacharys-MacBook-Pro.local>
2016-07-11 15:48:15 -0500

    added functionality

--- augment.js
+++ augment.js
@@ -1,14 +1,69 @@
 var CarLot = (function(iife) {
+	var garage = document.getElementById('garage');
+	var counter = 0;
 
-	var targetCard = ;
-	console.log(targetCard);
+	buildCar = function() {
+		var cars = getInventory();
+		cars.forEach(function (value, i) {
+			counter ++;
+			garage.innerHTML += `<div class='col-md-4 card' id='car${counter}'>`
+								+
+								"<h3>" + value.year + " " + value.make +
 
-	targetCard.addEventListener("click", cardClick);
+								"<h4>" + value.model + "</h4>" +
 
-	function cardClick() {
-		console.log("hello");
+								"</h3>" +
+								"<hr>" +
+
+								"<h5>" + "$" + value.price + "</h5>" +
+
+								"<img src=" + value.img + ">" +
+
+								"<p>" + 
+								value.description +
+								"</p>"
+								+
+								"</div>";
+
+
+		});
+
+	var cardStyle = document.getElementsByClassName("card");
+
+		for (var i = 0; i < 3; i++) {
+			var carColor = cars[i].color;
+			cardStyle[i].style.border = "5px solid";
+			cardStyle[i].style.borderColor = carColor;
+			cardStyle[i].style.background = "rgba(255, 255, 255, .1)"
+		}
+
+	for (var i = 0; i < cardStyle.length; i++) {
+		cardStyle[i].addEventListener("click", function(event) {
+			var targetClick = event.currentTarget;
+
+			for (i = 0; i < cardStyle.length; i++) {
+
+				if (event.target !== cardStyle[i]) {
+					cardStyle[i].style.borderWidth = "5px";
+
+				} else if (event.target === cardStyle[i]) {
+					targetClick.style.borderWidth = "8px";
+
+					if (event.target === targetClick) {
+						document.getElementById('textBox').focus();
+						textBox.addEventListener("keydown", function(press) {
+							if (press.keyCode === 13) {
+								targetClick.childNodes[5].innerHTML = "";
+								targetClick.childNodes[5].innerHTML = textBox.value;
+								textBox.value = "";
+							}
+						});
+					}
+					
+				}
+			}
+		});								
+	};
 	}
 
-
-
-}(CarLot || {}));
+})(CarLot);
--- quiz.js
+++ quiz.js
@@ -1,11 +1,9 @@
-// function populatePage (inventory) {
-//   // Loop over the inventory and populate the page
+var CarLot = (function(iife) {
 
-//   // Now that the DOM is loaded, establish all the event listeners needed
-//   CarLot.activateEvents();
-// }
+	function populatePage (inventory) {
+		buildCar();
+	}
 
-// // Load the inventory and send a callback function to be
-// // invoked after the process is complete
-// CarLot.loadInventory();
+	loadInventory(populatePage);
 
+})(CarLot);
